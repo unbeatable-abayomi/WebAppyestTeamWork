5a92508bb214ee689b5d2f6c675c7c4b7f29e524
abayomi igwubor <igwubor@outlook.com>
2020-06-21 11:51:04 +0100

    Add project files.

---
+++ WebAppyest/Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppyest.Controllers
+{
+    public class MainController : Controller
+    {
+
+        public IActionResult Resources()
+        {
+            return View();
+        }
+        public IActionResult ProjectLists()
+        {
+            return View();
+        }
+
+        public IActionResult Taskboard()
+        {
+            return View();
+        }
+
+        public IActionResult Activities()
+        {
+            return View();
+        }
+
+        public IActionResult Players()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
